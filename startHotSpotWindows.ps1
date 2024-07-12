ce6d0f807e10a8b9cf761e37b073163800b3e992
asebak <asebak@users.noreply.github.com>
2024-07-12 12:14:09 -0400

    Add files via upload

---
+++ startHotSpotWindows.ps1
@@ -0,0 +1,3 @@
+﻿$connectionProfile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetInternetConnectionProfile()
+$tetheringManager = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($connectionProfile)
+Await ($tetheringManager.StartTetheringAsync()) ([Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult])
